7a1b6fd189030635a7d09ec7a6e89018217f22e4
mbahbuy <mbahbagonku@gmail.com>
2024-08-12 13:53:30 +0700

    class seeder

--- database/seeders/BimbelSeeder.php
+++ database/seeders/BimbelSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bimbel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,34 @@ class BimbelSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Bimbel::create([
+            'title'         =>  'Matematika',
+            'description'   =>  'Class matematika',
+            'harga'         =>  75000,
+            'day_start'     =>  1,
+            'day_end'       =>  3,
+            'time_start'    =>  '14:00',
+            'time_end'      =>  '16:00',
+        ]);
+
+        Bimbel::create([
+            'title'         =>  'Fisika',
+            'description'   =>  'Class fisika',
+            'harga'         =>  100000,
+            'day_start'     =>  4,
+            'day_end'       =>  6,
+            'time_start'    =>  '14:00',
+            'time_end'      =>  '16:30',
+        ]);
+
+        Bimbel::create([
+            'title'         =>  'Laravel 11',
+            'description'   =>  'Class laravel versi 11',
+            'harga'         =>  200000,
+            'day_start'     =>  1,
+            'day_end'       =>  5,
+            'time_start'    =>  '08:00',
+            'time_end'      =>  '10:00',
+        ]);
     }
 }
